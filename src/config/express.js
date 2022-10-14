cade2b28ba66c9ef625d72f3a9c7a5d46ffb9de6
Daniel Barros <danielbarros338@gmail.com>
2022-10-14 16:30:41 -0300

    first commit

---
+++ src/config/express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname + "public/")));
+
+require("../routes/router")(app);
+
+module.exports = app;
