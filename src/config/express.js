22c8496c80c184a842017f475e5143d2b7ff151a
Daniel Barros <danielbarros338@gmail.com>
2022-10-15 00:37:20 -0300

    Primeira parte

--- src/config/express.js
+++ src/config/express.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname + "public/")));
+app.use(express.static(path.resolve("./public")));
 
 require("../routes/router")(app);
 
