1a1f389203cbafbb5807679025b98833be3665d8
Daniel Barros <danielbarros338@gmail.com>
2022-10-15 21:13:31 -0300

    rotate chevron

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,20 @@
 const btnDropdown = document.querySelectorAll(".btn-dropdown");
 const text = document.querySelectorAll(".accordeon article");
+const chevron = document.querySelectorAll(".accordeon span i");
 
 btnDropdown.forEach((btn, indexBtn) => {
   btn.addEventListener("click", () => {
     if (text[indexBtn].style.display === "block") {
       setSlideInUp(text[indexBtn].children[0].classList);
+
+      chevron[indexBtn].style.rotate = "0deg";
+
       setTimeout(() => {
         text[indexBtn].style.display = "none";
       }, 1000);
     } else {
       setSlideInDown(text[indexBtn].children[0].classList);
+      chevron[indexBtn].style.rotate = "180deg";
       text[indexBtn].style.display = "block";
     }
   });
