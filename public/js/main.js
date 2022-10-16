fbcb3f32806e620bc9ad7569c1992dee52023b91
Daniel Barros <danielbarros338@gmail.com>
2022-10-15 21:04:32 -0300

    Add efeito accordeon

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,40 @@
+const btnDropdown = document.querySelectorAll(".btn-dropdown");
+const text = document.querySelectorAll(".accordeon article");
 
+btnDropdown.forEach((btn, indexBtn) => {
+  btn.addEventListener("click", () => {
+    if (text[indexBtn].style.display === "block") {
+      setSlideInUp(text[indexBtn].children[0].classList);
+      setTimeout(() => {
+        text[indexBtn].style.display = "none";
+      }, 1000);
+    } else {
+      setSlideInDown(text[indexBtn].children[0].classList);
+      text[indexBtn].style.display = "block";
+    }
+  });
+});
+
+function setSlideInDown(classes) {
+  classes.forEach((cl) => {
+    if (cl === "animate__slideOutUp") {
+      classes.remove("animate__slideOutUp");
+      classes.remove("animate__animated");
+    }
+  });
+
+  classes.add("animate__animated");
+  classes.add("animate__slideInDown");
+}
+
+function setSlideInUp(classes) {
+  classes.forEach((cl) => {
+    if (cl === "animate__slideInDown") {
+      classes.remove("animate__slideInDown");
+      classes.remove("animate__animated");
+    }
+  });
+
+  classes.add("animate__animated");
+  classes.add("animate__slideOutUp");
+}
