6b087cdf4c71728728afe829d9a5c6c6caf1dffc
Daniel Barros <danielbarros338@gmail.com>
2022-10-20 09:38:59 -0300

    Coorrecao bug mensagens wpp

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 const btnDropdown = document.querySelectorAll(".btn-dropdown");
 const text = document.querySelectorAll(".accordeon article");
 const chevron = document.querySelectorAll(".accordeon span i");
-const fromSim = document.querySelector("#form-simulator");
+const formSim = document.querySelector("#form-simulator");
+const wantHire = document.querySelector("#want-hire");
+const talkWithUs = document.querySelector("#talk-with-us");
 const input = document.querySelector("#wpp-simulator-inp");
 
 btnDropdown.forEach((btn, indexBtn) => {
@@ -45,13 +47,25 @@ input.addEventListener("input", (e) => {
   }
 });
 
-fromSim.addEventListener("submit", (e) => {
+formSim.addEventListener("submit", (e) => {
   e.preventDefault();
   const wppUrl =
     "https://wa.me/558173166645?text=Gostaria%20de%20realizar%20uma%20simulação%20de%20Empréstimo%20com%20Garantia%20do%20Saque%20Aniversário%20do%20FGTS%20no%20valor%20de%20R$%20" +
     input.value;
   window.open(wppUrl);
 });
+
+wantHire.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const wppUrl = "https://wa.me/558173166645?text=Como%20posso%20fazer%20para%20contratar%20o%20Empréstimo%20com%20Garantia%20do%20Saque%20Aniversário%20do%20FGTS?"
+  window.open(wppUrl);
+})
+
+talkWithUs.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const wppUrl = "https://wa.me/558173166645?text=Gostaria%20de%20conhecer%20mais%20sobre%20a%20CredFácil"
+  window.open(wppUrl);
+})
 
 function setSlideInDown(classes) {
   classes.forEach((cl) => {
